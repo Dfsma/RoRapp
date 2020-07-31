9b5d3f3567b8a81caf71f9907308ebd9619906a1
Diego <diegof.df58@gmail.com>
2020-07-30 22:09:42 -0400

    env change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,8 +8,15 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+
+  
   # Show full error reports.
   config.consider_all_requests_local = true
 
