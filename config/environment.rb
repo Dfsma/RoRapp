e9b9c3b094d8fe7cd766c34cc07a12e36a84b76f
Diego <diegof.df58@gmail.com>
2020-07-30 23:26:28 -0400

    idk

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'apikey',
+    :password => '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',
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
