03918eda3cb1e9780e6826c5ae4a064b7714c4da
Diego <diegof.df58@gmail.com>
2020-08-09 16:14:57 -0400

    uh

--- db/schema.rb
+++ db/schema.rb
@@ -52,13 +52,13 @@ ActiveRecord::Schema.define(version: 2020_08_06_214041) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "permission_level"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
